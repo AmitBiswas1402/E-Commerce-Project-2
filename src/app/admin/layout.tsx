8cc20cea52e57a58018371abe41b90a5eaa0c887
Amit Biswas <amit.142biswas@gmail.com>
2024-07-10 22:12:33 +0530

    ecomm

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,23 @@
+import Nav, { NavLink } from '@/components/Nav';
+
+export const dynamic = 'force-dynamic'
+
+export default function layout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+  return (
+    <>
+        <Nav>
+          <NavLink href='/admin'>Dashboard</NavLink>
+          <NavLink href='/admin/products'>Products</NavLink>
+          <NavLink href='/admin/users'>Customer</NavLink>
+          <NavLink href='/admin/orders'>Orders</NavLink>
+            
+        </Nav>
+        <div className='container my-6'>{children}</div>
+    </>
+  )
+}
+
---
+++ src/app/admin/products/page.tsx
@@ -0,0 +1,6 @@
+export default function AdminProductpage() {
+    return (
+        <>
+        </>
+    )
+}
