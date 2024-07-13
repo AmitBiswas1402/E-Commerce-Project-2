0b149d0840626913f9095a5ef0f60dcc94123922
Amit Biswas <amit.142biswas@gmail.com>
2024-07-13 20:47:12 +0530

    utf

--- src/app/admin/_actions/product.ts
+++ src/app/admin/_actions/product.ts
@@ -1,6 +1,10 @@
 'use server'
 
+import db from '@/app/db/db';
 import {z} from 'zod';
+import fs from'fs/promises'
+import { redirect } from 'next/navigation';
+
 const fileSchema = z.instanceof(File, {message: "Required"})
 const imageSchema = fileSchema.refine(file => file.size === 0 || file.type.startsWith("image/"))
 
@@ -12,9 +16,32 @@ const addSchema = z.object({
     image: imageSchema.refine(file => file.size > 0, "Required")
 })
 
-export async function addProduct(formData: FormData) {
+export async function addProduct(prevState:unknown, formData: FormData) {
     const result = addSchema.safeParse(Object.fromEntries(formData.entries())) 
     if (result.success === false) {
         return result.error.formErrors.fieldErrors
     }
+
+    const data = result.data
+
+    await fs.mkdir("products", {recursive: true})
+    const filePath = `products/${crypto.randomUUID()} - ${data.file.name}`
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+
+    await fs.mkdir("public/products", {recursive: true})
+    const imagePath = `products/${crypto.randomUUID()} - ${data.file.name}`
+    await fs.writeFile(`public${imagePath}`, Buffer.from(await data.file.arrayBuffer()))
+
+    await db.product.create({ 
+        data: {
+            isAvailableForPurchase: false,
+            name: data.name,
+            description: data.description,
+            priceInCents: data.priceInCents,
+            filePath,
+            imagePath
+        }
+    })
+
+    redirect('/admin/products')
 }
